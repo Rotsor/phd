001ee9ce00571b7afca3beb5303d63c84132e302
Arseniy Alekseyev <rotsor@gmail.com>
2013-05-10 23:08:25 +0100

    Nix expression

--- breaker.hs
+++ breaker.hs
@@ -1,10 +1,14 @@
 import System.Environment
+import System.IO
 import Data.List.Split
 
+writeFileUtf8 f txt = withFile f WriteMode (\ hdl -> hSetEncoding hdl utf8 >> hPutStr hdl txt)
+
 main = do
+  hSetEncoding stdin utf8
   ["-o1", out1, "-o2", out2] <- getArgs
   let separator = "%%%%%%% BREAK HERE %%%%%%%"
   text <- getContents
   let [part1, part2] = splitOn separator text
-  writeFile out1 part1
-  writeFile out2 part2
+  writeFileUtf8 out1 part1
+  writeFileUtf8 out2 part2
---
+++ phd.nix
@@ -0,0 +1,29 @@
+let pkgs = import <nixpkgs> {}; in
+{ phd = pkgs.stdenv.mkDerivation {
+  name = "phd";
+  src = ./src;
+  breaker_hs = ./breaker.hs;
+  buildInputs = with pkgs; [texLiveFull ghostscript (haskellPackages.ghcWithPackages (self : with self; [haskellPlatform lhs2tex]))];
+  phases = [ "unpackPhase" "buildPhase" ];
+  buildPhase = ''
+	set -e
+	export BuildDir=`pwd`
+	lhs2TeX --agda agda.tex > agda-processed.tex
+# 	find -name '*.svg' 
+	runhaskell $breaker_hs -o1 agda-preamble.tex -o2 agda-processed-body.tex < agda-processed.tex
+	export TEXINPUTS=$BuildDir/styles:$BuildDir/DATE10_Balsa:$BuildDir/par_comp:
+	export BIBINPUTS=$BuildDir/par_comp:
+	echo RRR Latex 1
+	latex main
+	echo RRR Bibtex
+	bibtex main
+	echo RRR Latex 2
+	latex main
+	echo RRR Latex 3
+	latex main
+	echo RRR DVIPDF
+	dvipdf main.dvi
+	cp $BuildDir/main.pdf $out
+'';
+};
+}
